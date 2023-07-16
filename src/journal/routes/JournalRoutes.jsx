095fd5c72459f425f38227488820abce14660949
Nahuel Sanchez <mnahuelsanchez94@gmail.com>
2023-07-15 21:08:10 -0300

    routes 4th

--- src/journal/routes/JournalRoutes.jsx
+++ src/journal/routes/JournalRoutes.jsx
@@ -6,7 +6,7 @@ export const JournalRoutes = () => {
   return (
     <Routes>
       <Route path='/journal' element={<JournalPage /> } />
-      <Route path='/*' element={<Navigate to='/' /> } />
+      <Route path='/' element={<Navigate to='/' /> } />
     </Routes>
   )
 }
